141f8f4638f2041ac250dffa6c9c8de935d58960
khush <67963185+beyond-th0ught@users.noreply.github.com>
2022-11-11 11:02:52 +0530

    Create LestCommonAncesstor.java

---
+++ LestCommonAncesstor.java
@@ -0,0 +1,25 @@
+Node LCA(Node root, int n1, n2){
+    if(root == null) return null;
+    
+    if(root.data == n1 || root.data == n2) return node;
+    
+    Node left = LCA(node.left, n1, n2);
+    Node right = LCA(node.right, n1, n2);
+    
+    if(left != null) return left;
+    if(right != null) return right;
+    
+    return null;
+}
+
+
+Ex. BST
+
+          root ->     10
+                 1         4
+             -2      5  2       9
+             
+             
+             
+LCA(root, 5, 2)  ->   10  // 10 is lowest common node
+LCA(root, -1, 5)  ->  1   // 1  is lowest common node
